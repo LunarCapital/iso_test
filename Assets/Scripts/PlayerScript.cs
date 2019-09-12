52f1713192c1b9f813117d9cdf33ef7b6afe7586
Lunar Capital <lunarcapital@Dylans-MacBook-Air-1006.local>
2019-09-12 17:40:43 +0800

    movement testing

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -4,37 +4,27 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-    public float speed;             //Floating point variable to store the player's movement speed.
-    private Rigidbody2D rb2d;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
+	public float speed;             //Floating point variable to store the player's movement speed.
 
-    // Use this for initialization
-    void Start()
-    {
-        //Get and store a reference to the Rigidbody2D component so that we can access it.
-        rb2d = GetComponent<Rigidbody2D> ();
-    }
+	// Use this for initialization
+	void Start()
+	{
+	}
 
-    //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
-    void Update()
-    {
-		/*
-        //Store the current horizontal input in the float moveHorizontal.
-        float moveHorizontal = Input.GetAxis ("Horizontal");
+	//FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
+	void Update()
+	{
+		float inputX = Input.GetAxis("Horizontal");
+		float inputY = Input.GetAxis("Vertical");
 
-        //Store the current vertical input in the float moveVertical.
-        float moveVertical = Input.GetAxis ("Vertical");
+		Vector2 basespeed = new Vector2(50, 50);
+		Vector2 movement = new Vector2(
+				basespeed.x * inputX,
+				basespeed.y * inputY);
 
-        //Use the two store floats to create a new Vector2 variable movement.
-        Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
-
-		Debug.Log("H: " + moveHorizontal + ", V: " + moveVertical);
-
-        //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
-        rb2d.AddForce (movement * speed);
-		*/
-
+		GetComponent<Rigidbody2D>().velocity = movement;
+		//Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
 		
-		rb2d.velocity = new Vector2(Input.GetAxis("Horizontal") * speed * Time.deltaTime, Input.GetAxis("Vertical") * speed * Time.deltaTime);
-
+		//GetComponent<Rigidbody2D>().Move(transform.TransformDirection(input * speed * Time.deltaTime));
 	}
 }
